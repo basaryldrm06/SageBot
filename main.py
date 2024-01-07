255db7e1dfaac4665e46b3440e34ee6586f2549a
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-07 17:13:40 +0300

    main.py is implemented

---
+++ main.py
@@ -0,0 +1,74 @@
+from binance.client import Client
+from binanceAPI.position_utilities import enter_long, enter_short
+from config import api_key, secret_key
+from indicators.fetch_all_indicators import fetch_all_indicators
+from data.io_utilities import get_current_date_string, print_with_color, calculateWR
+from data.IndicatorData import IndicatorData
+from indicators.bar_prices import get_historical_data
+from data import io_utilities
+from time import sleep
+from data.data_functions import save_position, save_result
+from tensorflow.tensorflow_decision import predict
+import copy
+
+# Binance API initialization
+client = Client(api_key, secret_key)
+
+# Global Variables
+csv_path_position = "./data/sagebot_dataset.csv"
+csv_path_result = "./data/sagebot_result.csv"
+data_objects = [None, None]
+on_long = False
+on_short = False
+tp_price = 0
+sl_price = 0
+prediction = None
+tp_count = 0
+sl_count = 0
+
+# Global Functions
+def close_position(isTP):
+    global on_long
+    global on_short
+    global tp_count
+    global sl_count
+
+    on_long = False
+    on_short = False
+
+    if isTP:
+        print_with_color("green", "Position closed with TP")
+        print_with_color("yellow", "TP: " + tp_count, + " SL: " + 
+              sl_count + " win-rate" + calculateWR(tp_count, sl_count))
+    else:
+        print_with_color("red", "Position closed with SL")
+        print_with_color("yellow", "TP: " + tp_count, + " SL: " + 
+            sl_count + " win-rate" + calculateWR(tp_count, sl_count))
+        
+print_with_color("cyan", "SageBot is running...")
+
+while True:
+    try:
+        data_objects[1] = fetch_all_indicators(client)
+        if not (on_long or on_short):
+            data_objects[0] = copy.deepcopy(data_objects[1])
+            prediction = predict(csv_path_position, data_objects[0])
+            if prediction == "LONG":
+                tp_price, sl_price = enter_long(client) # TODO: change arguement of this function
+                on_long = True
+            elif prediction == "SHORT":
+                tp_price, sl_price = enter_short(client) # TODO: 2
+                on_short = True
+            print_with_color("yellow", "\nEntered " + prediction + " Current: " + 
+                             data_objects[0].price + " TP_PRICE: " + tp_price + 
+                             " SL_PRICE: " + sl_price)
+        else:
+            if (on_long and  data_objects[1].price > tp_price) or \
+                  (on_short and data_objects[1].price < tp_price):
+                close_position(True)
+            elif (on_long and data_objects[1].price < sl_price) or \
+                  (on_short and data_objects[1].price > sl_price):
+                close_position(False)
+            print("Position closed")
+    except:
+        pass
