bd7b4b765a95e20bbed6f7523c99246d6137a8ab
basaryldrm06 <basaryldrm06@gmail.com>
2024-01-08 15:41:25 +0300

    Tensorflow decision function is updated

--- tensorflow_utilities/tensorflow_decision.py
+++ tensorflow_utilities/tensorflow_decision.py
@@ -12,7 +12,7 @@ def predict(file_path, dataObj):
         if df.empty:
             raise ValueError("No data in csv file")
 
-        columns = ["date", "price", "macd_12", "macd_26", "ema_100", "rsi_6"]
+        columns = ["price", "macd_12", "macd_26", "ema_100", "rsi_6"]
         for i in range(0, 15):
             columns.extend([
                 f"opening_{i}", f"closing_{i}",
@@ -22,12 +22,13 @@ def predict(file_path, dataObj):
         X = df[columns].values
         y = df['state'].values
 
+        y = np.where(y == 'LONG', 1, 0)
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         model = Sequential()
         model.add(Dense(32, activation='relu', input_dim=X_train.shape[1]))
         model.add(Dense(1, activation='sigmoid'))
-
         model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
         model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
@@ -35,18 +36,20 @@ def predict(file_path, dataObj):
         _, test_acc = model.evaluate(X_test, y_test)
 
         def predict(indicatorDataObj):
-            features = [indicatorDataObj.date, indicatorDataObj.price, 
-                        indicatorDataObj.macd_12, indicatorDataObj.macd_26, 
-                        indicatorDataObj.ema_100, indicatorDataObj.rsi_6]
+            features = [float(indicatorDataObj.price),
+                        float(indicatorDataObj.macd_12), float(indicatorDataObj.macd_26),
+                        float(indicatorDataObj.ema_100), float(indicatorDataObj.rsi_6)]
+
             for bar in indicatorDataObj.bar_list:
-                for element in bar: 
-                    features.append(element)
+                for element in bar:
+                    features.append(float(element))
 
             new_data = np.array([features])
-            predictions = model.predict(new_data)
+            prediction_prob = model.predict(new_data)
 
-            predictions = np.where(predictions > 0.5, "LONG", "SHORT")
-            return predictions
+            prediction = "LONG" if prediction_prob > 0.5 else "SHORT"
+
+            return prediction
 
         prediction = predict(dataObj)
         return test_acc, prediction
